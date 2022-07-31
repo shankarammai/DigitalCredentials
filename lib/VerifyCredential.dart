c3db24886adc971840110f84ca1ebaf0378cdb32
Shankar Ammai <ammai.shankar@gmail.com>
2022-07-31 20:32:44 +0100

    Verifying credential through QR

--- lib/VerifyCredential.dart
+++ lib/VerifyCredential.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pointycastle/asymmetric/api.dart';
 import 'package:uuid/uuid.dart';
 import 'services/secure_storage.dart';
 import 'dart:developer' as developer;
@@ -9,8 +12,11 @@ import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'ChooseIssuer.dart';
 import 'widgets/CustomWidget.dart';
-import 'package:barcode_widget/barcode_widget.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+// import 'package:barcode_widget/barcode_widget.dart';
+// import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:encrypt/encrypt.dart' as cryptolib;
+import 'package:cool_alert/cool_alert.dart';
 
 class VerifyCredential extends StatefulWidget {
   VerifyCredential({Key? key}) : super(key: key);
@@ -19,57 +25,183 @@ class VerifyCredential extends StatefulWidget {
   State<VerifyCredential> createState() => _VerifyCredentialState();
 }
 
-
 class _VerifyCredentialState extends State<VerifyCredential> {
-  String _scanBarcode = 'Unknown';
-  Future<void> scanQR() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.QR);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
+  String scanBarcode = 'Unknown';
+  QRViewController? controller;
+  Barcode? result;
+  bool showCamera = true;
+  bool showWidgets = false;
+  late Map jsonQR;
+  late Map credentialDoc;
+  String? decryptionKey;
+  String? presentedCredentialData;
+  List<Widget> documentWidgets = [Text('data')];
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  void scanQR(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((event) {
+      setState(() {
+        showCamera = false;
+        try {
+          jsonQR = json.decode(event.code.toString());
 
-    setState(() {
-      _scanBarcode = barcodeScanRes;
+          //get the id and key
+          String documentId = jsonQR['documentId'];
+          decryptionKey = (jsonQR['decryptionKey']);
+          print('Document Id >>>>>>>>>>>>>>' + documentId);
+          //get data from service provider
+          Future<DocumentSnapshot<Map<String, dynamic>>> document =
+              FirebaseFirestore.instance
+                  .collection("ShowCredential")
+                  .doc(documentId)
+                  .get();
+          document.then((DocumentSnapshot documentSnapshot) {
+            if (documentSnapshot.exists) {
+              credentialDoc = documentSnapshot.data() as Map<String, dynamic>;
+              print(credentialDoc['credentialData']);
+
+              //decrypt it
+              final encrypter = cryptolib.Encrypter(cryptolib.AES(
+                  cryptolib.Key.fromUtf8(this.decryptionKey.toString()),
+                  mode: cryptolib.AESMode.cbc));
+              final base64encData = cryptolib.Encrypted.fromBase64(
+                  credentialDoc['credentialData']['data']);
+              final iv = 'ThisIsASecuredBlock'.substring(0, 16);
+              presentedCredentialData = encrypter.decrypt(base64encData,
+                  iv: cryptolib.IV.fromUtf8(iv));
+              print(presentedCredentialData);
+
+              //Make widget to show the credentails
+              Map<String, dynamic> credentialDataJson =
+                  json.decode(presentedCredentialData!);
+              documentWidgets=[];
+              credentialDataJson.forEach((key, value) {
+                documentWidgets
+                    .add(Divider(height: 30.0, color: Colors.grey[800]));
+                documentWidgets.add(Text(
+                  key.toUpperCase(),
+                  style: const TextStyle(
+                    color: Colors.orange,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ));
+
+                documentWidgets.add(Text(
+                  value,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ));
+                print(documentWidgets);
+                print(key);
+              });
+              documentWidgets.add(ElevatedButton(
+                child: const Text('Verify Document'),
+                onPressed: () {
+
+                  var verifierPublicKey =
+                  cryptolib.RSAKeyParser().parse(credentialDoc['issuer']['publicKey']) as RSAPublicKey;
+                  var signer=cryptolib.Signer(cryptolib.RSASigner(cryptolib.RSASignDigest.SHA256,publicKey: verifierPublicKey));
+                  bool signResult=signer.verify64(presentedCredentialData!, credentialDoc['proof']['proofValue']);
+                  print(credentialDoc['proof']['proofValue']);
+                  print("Signature Result >> " + signResult.toString());
+
+                  // showDialog(
+                  //     context: context,
+                  //     builder: (_) => const AlertDialog(
+                  //       title: Text('Verification'),
+                  //       content: Text('All fields should be filled'),
+                  //     )
+                  // );
+                  if(signResult){
+                  CoolAlert.show(
+                    context: context,
+                    animType:CoolAlertAnimType.rotate,
+                    title: "Verified",
+                    type: CoolAlertType.success,
+                    widget: Text('Issued by: '+ credentialDoc['issuer']['name']),
+                    //Retrive from Database to check whose public key is it
+                  );
+                  }
+                  else{
+                    CoolAlert.show(
+                      context: context,
+                      animType:CoolAlertAnimType.rotate,
+                      title: "Invalid",
+                      type: CoolAlertType.error,
+                      text: "Signature Not Valid",
+                    );
+
+                  }
+
+
+
+
+                },
+              ),);
+              
+              setState(() {});
+            }
+          });
+
+          //check signature & verify it
+
+        } catch (e) {
+          print(e);
+          scanBarcode = "Invalid QR for this application";
+          showCamera = false;
+          showWidgets = true;
+        }
+        print(documentWidgets);
+
+        scanBarcode = "Done";
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Choose Issuer'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
+      appBar: AppBar(
+        title: const Text('Verify Credential'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ListView(children: [
+          Column(
             children: [
-              Text(
-                  'Providing my details to the holder'),
-              BarcodeWidget(
-                barcode: Barcode.qrCode(errorCorrectLevel: BarcodeQRCorrectionLevel.high), // Barcode type and settings
-                data: 'https://pub.dev/packages/barcode_widget', // Content
-                width: 200,
-                height: 200,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  scanQR();
-                },
-                child: const Text('Click Here to Read Data'),
-              ),
-
+              Text(this.scanBarcode),
+              Visibility(
+                  visible: showCamera,
+                  child: Container(
+                      height: 200,
+                      width: MediaQuery.of(context).size.width - 100,
+                      child:
+                          QRView(key: GlobalKey(), onQRViewCreated: scanQR))),
+              Column(children: documentWidgets)
             ],
           ),
-        ));
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          setState(() {
+            showCamera = true;
+          });
+        },
+        icon: Icon(
+          Icons.qr_code_scanner,
+          size: 29,
+        ),
+        backgroundColor: Colors.cyan,
+        label: const Text('Start Scan'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
--- lib/ViewCredential.dart
+++ lib/ViewCredential.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -10,8 +13,6 @@ import "package:pointycastle/export.dart" as pointyCastle;
 import 'package:verifiable_credentials/services/file_read_write.dart';
 import 'package:verifiable_credentials/services/key_generatation.dart';
 import 'package:verifiable_credentials/services/secure_storage.dart';
-
-
 
 class ViewCredential extends StatefulWidget {
   final String credentailDocument;
@@ -25,11 +26,16 @@ class ViewCredential extends StatefulWidget {
 
 class _ViewCredentialState extends State<ViewCredential> {
   final String credentialDocument;
-  String? publicKeyPEM, privateKeyPEM, Uuid, fileContents,sharedKey;
-  RSAPublicKey?  holderPublicKey, issuerPublicKey;
+  late String publicKeyPEM,
+      privateKeyPEM,
+      Uuid,
+      fileContents,
+      sharedKey,
+      credentialData;
+  RSAPublicKey? holderPublicKey, issuerPublicKey;
   RSAPrivateKey? holderPrivateKey;
-
-
+  List selectedFields = [];
+  late Map<String, dynamic> credentialDataJson, credentialDocumentJson;
 
   final SecureStorage secureStorage = SecureStorage();
 
@@ -39,78 +45,236 @@ class _ViewCredentialState extends State<ViewCredential> {
     return (await secureStorage.readSecureData(Key)).toString();
   }
 
-  Future<List> _load_Details() async{
+  Future<List> _load_Details() async {
     await _getDataFromStorage('publicKeyPEM').then((value) {
-        publicKeyPEM = value.toString();
+      publicKeyPEM = value.toString();
     });
     await _getDataFromStorage('Uuid').then((value) {
-        Uuid = value.toString();
+      Uuid = value.toString();
     });
     await _getDataFromStorage('privateKeyPEM').then((value) {
-        privateKeyPEM = value.toString();
+      privateKeyPEM = value.toString();
     });
-    await readFile('credentials/'+this.credentialDocument).then((value) {
-        fileContents = value.toString();
+    await readFile('credentials/' + this.credentialDocument).then((value) {
+      fileContents = value.toString();
     });
 
-    return [privateKeyPEM,publicKeyPEM,Uuid,fileContents];
-
+    return [privateKeyPEM, publicKeyPEM, Uuid, fileContents];
   }
 
   @override
   void initState() {
     super.initState();
-    dynamic _getDetails=_load_Details().whenComplete(() {
-      holderPublicKey = cryptolib.RSAKeyParser().parse(this.publicKeyPEM!) as RSAPublicKey;
-      holderPrivateKey = cryptolib.RSAKeyParser().parse(this.privateKeyPEM!) as RSAPrivateKey;
+    dynamic _getDetails = _load_Details().whenComplete(() {
+      holderPublicKey =
+          cryptolib.RSAKeyParser().parse(this.publicKeyPEM) as RSAPublicKey;
+      holderPrivateKey =
+          cryptolib.RSAKeyParser().parse(this.privateKeyPEM) as RSAPrivateKey;
 
-      var jsonFile=json.decode(this.fileContents!);
-      var data_enc=jsonFile['credentialData']['data'];
-      var sharedKey_enc=jsonFile['credentialData']['encryptionKey'];
+      credentialDocumentJson = json.decode(this.fileContents);
+      var data_enc = credentialDocumentJson['credentialData']['data'];
+      var sharedKey_enc = credentialDocumentJson['credentialData']['encryptionKey'];
 
       //Retrive the sharedKey
-      final encryptDecrypt=cryptolib.Encrypter(cryptolib.RSA(publicKey: holderPublicKey,privateKey: holderPrivateKey,encoding: cryptolib.RSAEncoding.PKCS1));
-      sharedKey=encryptDecrypt.decrypt(cryptolib.Encrypted.fromBase64(sharedKey_enc));
+      final encryptDecrypt = cryptolib.Encrypter(cryptolib.RSA(
+          publicKey: holderPublicKey,
+          privateKey: holderPrivateKey,
+          encoding: cryptolib.RSAEncoding.PKCS1));
+      sharedKey =
+          encryptDecrypt.decrypt(cryptolib.Encrypted.fromBase64(sharedKey_enc));
       print(sharedKey);
-      
-      final encrypter = cryptolib.Encrypter(cryptolib.AES(cryptolib.Key.fromUtf8(this.sharedKey!),mode:cryptolib.AESMode.cbc));
-      String decryptedData=encrypter.decrypt64(data_enc,iv: cryptolib.IV.fromLength(16));
-      print(decryptedData);
 
-      setState(() {});
+      final encrypter = cryptolib.Encrypter(cryptolib.AES(
+          cryptolib.Key.fromUtf8(this.sharedKey),
+          mode: cryptolib.AESMode.cbc));
+      final base64encData = cryptolib.Encrypted.fromBase64(data_enc);
+      final iv = 'ThisIsASecuredBlock'.substring(0, 16);
+      credentialData =
+          encrypter.decrypt(base64encData, iv: cryptolib.IV.fromUtf8(iv));
+      print(credentialData);
+
+      // setState(() {});
     });
+  }
+
+  _show_selected_qr(){
+
+  }
+
+  _show_full_qr(){
+    // showDialog(context: context,
+    //     builder: (_) =>
+    //     AlertDialog(
+    //   title: Text('Scan this QR'),
+    //   content: Container(
+    //       height:MediaQuery.of(context).size.height,
+    //       width: MediaQuery.of(context).size.width,
+    //       child:
+    //     BarcodeWidget(
+    //       barcode: Barcode.qrCode(errorCorrectLevel: BarcodeQRCorrectionLevel.high), // Barcode type and settings
+    //       data: 'https://pub.dev/packages/barcode_widget', // Content
+    //       width: 200,
+    //       height: 200,
+    //     )
+    //     ),
+    // ));
+   var sendData=credentialDocumentJson;
+   sendData.remove('credentialData');
+   Map<String, dynamic> credentialData_map=Map();
+
+   //Encrypting information
+   //random String
+   String verifierEnckey=String.fromCharCodes(List.generate(16, (index) => Random().nextInt(33) + 89));
+   final encrypter = cryptolib.Encrypter(cryptolib.AES(
+       cryptolib.Key.fromUtf8(verifierEnckey),
+       mode: cryptolib.AESMode.cbc));
+   final iv = 'ThisIsASecuredBlock'.substring(0, 16);
+   String enc_Credential= encrypter.encrypt(credentialData,iv: cryptolib.IV.fromUtf8(iv)).base64;
+
+   dynamic credentailDetails={'data':enc_Credential};//,'encryptionKey':verifierEnckey,'encryptionType':'AES-128-CBC'};
+
+   credentialData_map['credentialData']= credentailDetails;
+
+   sendData.addAll(credentialData_map);
+   print(sendData['credentialData']);
+
+   CollectionReference showCredentials = FirebaseFirestore.instance.collection('ShowCredential');
+   showCredentials.add(sendData).then((value) => {
+   Navigator.of(context).push(MaterialPageRoute<Null>(
+   builder: (BuildContext context) {
+     Map<String, dynamic> dataForQR={'decryptionKey':verifierEnckey,'documentId':value.id};
+     print(jsonEncode(dataForQR));
+   return AlertDialog(
+   title: Text('Scan this QR'),
+   content: Container(
+   height:MediaQuery.of(context).size.height/2,
+   width: MediaQuery.of(context).size.width,
+   child:
+   BarcodeWidget(
+   barcode: Barcode.qrCode(errorCorrectLevel: BarcodeQRCorrectionLevel.high), // Barcode type and settings
+   data: jsonEncode(dataForQR), // Content
+   width: 200,
+   height: 200,
+   )
+   ),
+   actions: [TextButton(
+   onPressed: () => Navigator.pop(context),
+   child: Text('DONE'),
+   ),]
+   ,
+   );
+   }
+   ))
+
+   });
+
+
+
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('View Credential')),
-        body: Center(
-          child:FutureBuilder<List>(
-            future: _load_Details(),
-            builder: (
-                BuildContext context,
-                AsyncSnapshot<List> snapshot,
-                ) {
-              print(snapshot.connectionState);
+        body: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.blue),
+                    onPressed: _show_selected_qr,
+                  child: const Text('Show Selected '),
+                ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                    onPressed: _show_full_qr,
+                    child: const Text('Show Full '),
+                  ),
+                ],
+              ),
+              FutureBuilder<List>(
+          future: _load_Details(),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<List> snapshot,
+          ) {
+            print(snapshot.connectionState);
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return const Text('Error');
-                } else if (snapshot.hasData) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return const Text('Error');
+              } else if (snapshot.hasData) {
+                // credentialDataJson = json.decode((this
+                //         .credentialData
+                //         .substring(1, this.credentialData.length - 1))
+                //     .toString());
+                credentialDataJson = json.decode((this.credentialData));
+                return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:credentialDataJson.entries.map((e) {
+                  return Wrap(
 
-                      return ListView(children: [Text((snapshot.data).toString())]);
-                } else {
-                  return const Text('Empty data');
-                }
+                    children: [
+                      Divider(
+                        height: 30.0,
+                        color: Colors.grey[800],
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                              value: this.selectedFields.contains(e.key),
+                              onChanged: (newValue) {
+                                if (newValue == true) {
+                                  setState(() {
+                                    this.selectedFields.add(e.key);
+                                  });
+                                } else {
+                                  setState(() {
+                                    this.selectedFields.remove(e.key);
+                                  });
+                                }
+                                print(this.selectedFields);
+                              }),
+                          Text(
+                            e.key.toUpperCase(),
+                            style: TextStyle(
+                              color: Colors.orange,
+                              letterSpacing: 1.0,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        e.value,
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2.0,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  );
+                }).toList());
               } else {
-                return Text('State: ${snapshot.connectionState}');
+                return const Text('Empty data');
               }
-            },
-          )
+            } else {
+              return Text('State: ${snapshot.connectionState}');
+            }
+          },
         )
+    ])
     );
   }
 }
